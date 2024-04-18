45bddf102ac119f98fef4077011de9c5a6e8ec74
aureateAnatidae <aureateAnatidae@gmail.com>
2024-04-17 20:02:35 -0600

    basic pygame window, requirements.txt

--- __main__.py
+++ __main__.py
@@ -1,3 +1,28 @@
 # Load all cards from two decks
 # TODO: json card system
+
 # Open game window
+
+import pygame
+
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
